c3bc95559900ad07244cfe4ff09d5522988fecea
Ryan Haworth <rhaworth211@msn.com>
2025-06-23 11:48:19 -0400

    Display updates

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -15,7 +16,7 @@ export class LoginComponent {
   password = '';
 
   constructor(private auth: AuthService, private router: Router) {}
-  
+
   /**
    * Handles user login by calling AuthService.login.
    * On successful login, navigates to the patient list page.
